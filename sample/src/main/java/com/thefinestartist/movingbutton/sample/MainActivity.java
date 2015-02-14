a48b9644553c828046c047cfc56bd1e0c063ce6b
TheFinestArtist <contact@thefinestartist.com>
2015-02-14 09:39:04 +0900

    Update Move Direction & vibration settings

--- sample/src/main/java/com/thefinestartist/movingbutton/sample/MainActivity.java
+++ sample/src/main/java/com/thefinestartist/movingbutton/sample/MainActivity.java
@@ -10,6 +10,7 @@ import com.afollestad.materialdialogs.MaterialDialog;
 import com.thefinestartist.movingbutton.MovingButton;
 import com.thefinestartist.movingbutton.enums.ButtonPosition;
 import com.thefinestartist.movingbutton.enums.MoveDirection;
+import com.thefinestartist.movingbutton.enums.VibrationStrength;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -23,6 +24,14 @@ public class MainActivity extends ActionBarActivity {
     MovingButton movingButton;
     @InjectView(R.id.current_position_info)
     TextView currentPositionInfo;
+    @InjectView(R.id.move_direction_bt)
+    View moveDirectionBt;
+    @InjectView(R.id.move_direction_tv)
+    TextView moveDirectionTv;
+    @InjectView(R.id.vibration_strength_bt)
+    View vibrationStrengthBt;
+    @InjectView(R.id.vibration_strength_tv)
+    TextView vibrationStrengthTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,26 +47,59 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
-        new MaterialDialog.Builder(this)
-                .title("Move Direction")
-                .items(names())
-                .itemsCallbackSingleChoice(-1, new MaterialDialog.ListCallback() {
-                    @Override
-                    public void onSelection(MaterialDialog dialog, View view, int which, CharSequence text) {
-                    }
-                })
-                .positiveText("OK")
-                .show();
+        moveDirectionBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialDialog.Builder(MainActivity.this)
+                        .title(getString(R.string.move_direction))
+                        .items(getMoveDirectionNames())
+                        .itemsCallbackSingleChoice(movingButton.getMoveDirection().ordinal(), new MaterialDialog.ListCallback() {
+                            @Override
+                            public void onSelection(MaterialDialog dialog, View view, int which, CharSequence text) {
+                                MoveDirection moveDirection = MoveDirection.values()[which];
+                                movingButton.setMoveDirection(moveDirection);
+                                moveDirectionTv.setText(moveDirection.name());
+                            }
+                        })
+                        .positiveText(getString(R.string.choose))
+                        .show();
+
+            }
+        });
+
+        vibrationStrengthBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialDialog.Builder(MainActivity.this)
+                        .title(getString(R.string.vibration_strength))
+                        .items(getVibrationStrengthNames())
+                        .itemsCallbackSingleChoice(movingButton.getVibrationStrength().ordinal(), new MaterialDialog.ListCallback() {
+                            @Override
+                            public void onSelection(MaterialDialog materialDialog, View view, int which, CharSequence text) {
+                                VibrationStrength vibrationStrength = VibrationStrength.values()[which];
+                                movingButton.setVibrationStrength(vibrationStrength);
+                                vibrationStrengthTv.setText(vibrationStrength.name());
+                            }
+                        })
+                        .positiveText(getString(R.string.choose))
+                        .show();
+            }
+        });
     }
 
-    public static String[] names() {
+    public static String[] getMoveDirectionNames() {
         MoveDirection[] states = MoveDirection.values();
         String[] names = new String[states.length];
-
-        for (int i = 0; i < states.length; i++) {
+        for (int i = 0; i < states.length; i++)
             names[i] = states[i].name();
-        }
+        return names;
+    }
 
+    public static String[] getVibrationStrengthNames() {
+        VibrationStrength[] states = VibrationStrength.values();
+        String[] names = new String[states.length];
+        for (int i = 0; i < states.length; i++)
+            names[i] = states[i].name();
         return names;
     }
 }
